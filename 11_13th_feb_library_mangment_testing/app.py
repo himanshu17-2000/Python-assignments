0297b114369de8e92cfd1bd63b23b23c7cc3af59
Himanshu <earthboxer@gmail.com>
2023-03-01 17:14:19 +0530

    Updated the application

--- 11_13th_feb_library_mangment_testing/app.py
+++ 11_13th_feb_library_mangment_testing/app.py
@@ -1,9 +1,8 @@
 from init_app import create_app
 from sqlobject import *
 import os
-
-
+from models.Member import Member
+from models.Transaction import Transaction
 app = create_app()
-
 if __name__ == "__main__":
     app.run(debug=True)
